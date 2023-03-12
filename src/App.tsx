4f0684de27c624573634c8cc58b9aaabd7d630d4
Philippe de Bekker <P.M.deBekker@student.tudelft.nl>
2023-03-12 21:19:57 +0100

    Fix mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Container, Navbar, Form, FormControl, Button, OverlayTrigger, Tooltip }
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
-import logo from '../public/susie.svg';
+import logo from '/susie.svg';
 
 function App() {
   return (
@@ -22,8 +22,8 @@ function App() {
         </Container>
       </Navbar>
       <Container className="d-flex flex-column justify-content-center align-items-center my-5">
-        <p className="text-center font-weight-bold mb-4" style={{ fontSize: '20px'}}>
-          Analyze the sustainability of a repository 🌍💞
+        <p className="text-center font-weight-bold mb-4" style={{ fontSize: '18px'}}>
+          How sustainable is your GitHub repository? 🦖📟
         </p>
         <div className="d-flex align-items-center">
           <Form className="d-flex">
@@ -31,13 +31,13 @@ function App() {
               placement="bottom"
               overlay={<Tooltip>Enter a GitHub repository URL to analyze sustainability</Tooltip>}
             >
-              <FormControl type="text" placeholder="Paste GitHub URL here" className="rounded-pill bg-white text-dark border-0 shadow-sm w-100 mr-2" style={{ maxWidth: '800px', minWidth: '400px' }} />
+              <FormControl type="text" placeholder="Paste GitHub URL here" className="rounded-pill bg-white text-dark border-0 shadow-sm w-100 mr-2" style={{ maxWidth: '90%', minWidth: '400px', borderColor: '#757167', borderWidth: 'medium' }} />
             </OverlayTrigger>
             <OverlayTrigger
               placement="bottom"
               overlay={<Tooltip>Analyze</Tooltip>}
             >
-              <Button variant="primary" className="rounded-pill" style={{ backgroundColor: '#007bff', marginLeft: '10px' }}>
+              <Button variant="secondary" className="rounded-pill" style={{ backgroundColor: '#757167', marginLeft: '10px' }}>
                 <FontAwesomeIcon icon={faSearch} />
               </Button>
             </OverlayTrigger>

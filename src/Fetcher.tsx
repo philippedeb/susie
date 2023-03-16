ad07ddac80257abc028ccffd6f1b285e05481cad
Sebastien van Tiggele <sebastien99@hotmail.nl>
2023-03-16 17:02:01 +0100

    Add get dates function

--- src/Fetcher.tsx
+++ src/Fetcher.tsx
@@ -5,9 +5,10 @@ export {}
 export async function getData(searchValue: string): Promise<void> {
     try {
         const repo = extractGitHubRepoPath(searchValue)
-        getBranches(repo);
-        getCommits(repo);
-        getPullRequests(repo);
+        // getBranches(repo);
+        // getCommits(repo);
+        // getPullRequests(repo);
+        getDates(repo);
     } catch (error) {
         console.error(error);
     }
@@ -21,8 +22,10 @@ interface GitCommit {
   commit: {
     message: string
     author: {
+      name: string,
       date: string
     }
+    date: string
   }
 }
 
@@ -35,7 +38,7 @@ async function getBranches(repo: string): Promise<string[]> {
     const response = await fetch('https://api.github.com/repos/' + repo + '/branches');
     const data: GitBranch[] = await response.json();
     const branchNames = data.map((item) => item.name.toLowerCase());
-    console.log("Branches Found");
+    // console.log("Branches Found");
     return branchNames
   } catch (error) {
     console.error(error);
@@ -48,7 +51,7 @@ async function getPullRequests(repo: string): Promise<string[]> {
     const response = await fetch('https://api.github.com/repos/' + repo + '/pulls');
     const data: GitPull[] = await response.json();
     const pullNames = data.map((item) => item.title.toLowerCase());
-    console.log("Pull Requests Found");
+    // console.log("Pull Requests Found");
     return pullNames
   } catch (error) {
     console.error(error);
@@ -58,15 +61,40 @@ async function getPullRequests(repo: string): Promise<string[]> {
 
 async function getCommits(repo: string): Promise<string[]> {
   try {
-    const response = await fetch('https://api.github.com/repos/' + repo + '/commits');
+    const response = await fetch('https://api.github.com/repos/' + repo + '/commits?page=1&per_page=100');
     const data: GitCommit[] = await response.json();
     const commitNames = data.map((item) => item.commit.message.toLowerCase());
+    const commitDates = data.map((item) => item.commit.author.date);
     console.log("Commits Found");
+    console.log(commitDates);
     return commitNames
   } catch (error) {
     console.error(error);
     return ["No Commits Found"];
   }
+}
+
+async function getDates(repo: string): Promise<string[]> {
+  let n: number = 1;
+  let dateList: string[] = []
+  while (n < 5) {
+    try {
+      const link: string = 'https://api.github.com/repos/' + repo + '/commits?page=' + n.toString();
+      console.log(link)
+      const response = await fetch(link)
+      const data: GitCommit[] = await response.json();
+      const commitDates = data.map((item) => item.commit.author.date);
+      dateList = dateList.concat(commitDates)
+      if (commitDates.length < 30) {
+        break;
+      }
+      n++
+    } catch (error) {
+      console.error(error);
+      return ["No Dates Found"];
+    }
+  }
+  return dateList;
 }
 
 /**

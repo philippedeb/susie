e31cbbc50bc951871e42d636055d84cc9ceb0cc1
Philippe de Bekker <P.M.deBekker@student.tudelft.nl>
2023-03-16 18:43:55 +0100

    Add animation Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import SearchBar from "./SearchBar";
+import "../css/Home.css";
 
 interface Props {
   onSearch: (value: string) => void;
 }
 
 function Home(props: Props) {
+  const [loaded, setLoaded] = useState(false);
+
   function handleSearch(value: string) {
     console.log(`Searching for ${value} in Home.tsx...`);
     props.onSearch(value);
   }
 
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
-    <Container className="d-flex flex-column justify-content-center align-items-center my-5">
+    <Container
+      className={`d-flex flex-column justify-content-center align-items-center my-5${
+        loaded ? " loaded" : ""
+      }`}
+    >
       <p
         className="text-center font-weight-bold mb-4"
         style={{ fontSize: "18px" }}

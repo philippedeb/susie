7bea1e43f570d65d4a304302636c769eaaf2b8ef
Merel Steenbergen <m.a.steenbergen@student.tudelft.nl>
2023-03-17 09:04:38 +0100

    Fill table of programming language energy usage

--- src/LanguageScores.tsx
+++ src/LanguageScores.tsx
@@ -2,8 +2,7 @@ export interface LanguageScoreInterface {
     data: Record<string, number>;
 }
 
-// https://medium.com/codex/what-are-the-greenest-programming-languages-e738774b1957
-// TODO: Fill this
+// https://greenlab.di.uminho.pt/wp-content/uploads/2017/10/sleFinal.pdf
 export const LanguageScores: LanguageScoreInterface = {
     data: {
         "C": 1, 
@@ -11,6 +10,27 @@ export const LanguageScores: LanguageScoreInterface = {
         "C++": 1.34,
         "Ada": 1.70,
         "Java": 1.98,
-        "Ruby": 69.91
+        "Pascal": 2.14,
+        "Chapel": 2.18, 
+        "Lisp": 2.27,
+        "Ocaml": 2.40,
+        "Fortran": 2.52,
+        "Swift": 2.79,
+        "Haskell": 3.10,
+        "C#": 3.14,
+        "Go":3.23,
+        "Dart":3.83,
+        "F#": 4.13,
+        "JavaScript":4.45,
+        "Racket": 7.91,
+        "TypeScript": 21.50,
+        "Hack":24.02,
+        "PHP": 29.30,
+        "Erlang":42.23,
+        "Lua":45.98,
+        "Jruby":46.54,
+        "Ruby":69.91, 
+        "Python":75.88,
+        "Perl":79.58
     }
 };

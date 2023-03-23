40295ef63cbd228980e45e1f4824f5d1d45d381e
Merel Steenbergen <m.a.steenbergen@student.tudelft.nl>
2023-03-23 09:55:50 +0100

    Fix percentage calculation

--- src/components/metrics/Workflows/workflow-analyser.tsx
+++ src/components/metrics/Workflows/workflow-analyser.tsx
@@ -14,8 +14,8 @@ function WorkflowAnalysis(props: Props) {
     for (var i = 0, j = props.statusses.length; i < j; i++) {
        occurrences[props.statusses[i]] = (occurrences[props.statusses[i]] || 0) + 1;
     }
-    const percentage_success = occurrences['failure']/props.statusses.length * 100
-    if (percentage_success > THRESHOLD) {
+    const percentage_failures = occurrences['failure']/props.statusses.length * 100
+    if (percentage_failures > THRESHOLD) {
         console.log("Your builds are failing quite often, you should try running locally before pushing!")
     } else {
         console.log("Your builds are successful. Running locally before pushing saves energy, so good job!")

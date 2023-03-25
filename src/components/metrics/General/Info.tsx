8841759409ebc3ff4559275124ef1149c8f8bc93
Philippe de Bekker <P.M.deBekker@student.tudelft.nl>
2023-03-25 19:07:28 +0100

    Add warning when max capacity is reached

--- src/components/metrics/General/Info.tsx
+++ src/components/metrics/General/Info.tsx
@@ -1,3 +1,5 @@
+import { Alert } from "react-bootstrap";
+
 interface Props {
   commits: number;
   pullRequests: number;
@@ -6,21 +8,42 @@ interface Props {
 }
 
 function Info(props: Props) {
+  const possiblyMaxCapacityReached =
+    props.commits == 100 ||
+    props.pullRequests == 100 ||
+    props.branches == 100 ||
+    props.issues == 100;
+
   return (
-    <ul>
-      <li>
-        <strong>Commits:</strong> {props.commits}
-      </li>
-      <li>
-        <strong>Pull Requests:</strong> {props.pullRequests}
-      </li>
-      <li>
-        <strong>Branches:</strong> {props.branches}
-      </li>
-      <li>
-        <strong>Issues:</strong> {props.issues}
-      </li>
-    </ul>
+    <>
+      <ul>
+        <li>
+          <strong>Commits:</strong> {props.commits}
+        </li>
+        <li>
+          <strong>Pull Requests:</strong> {props.pullRequests}
+        </li>
+        <li>
+          <strong>Branches:</strong> {props.branches}
+        </li>
+        <li>
+          <strong>Issues:</strong> {props.issues}
+        </li>
+      </ul>
+      {possiblyMaxCapacityReached && (
+        <>
+          <Alert variant="warning">
+            {" "}
+            ⚠️ The maximum load capacity of 100 is reached; any further
+            information is not included in the analysis.{" "}
+          </Alert>
+          <Alert variant="secondary">
+            🎯 While not all past data might be relevant anyway, we are planning
+            on releasing this feature soon!
+          </Alert>
+        </>
+      )}
+    </>
   );
 }
 
